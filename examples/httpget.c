addb5251e06b20b8abd01540e0d5b45f8cd840db
Daniel-Constantin Mierla <miconda@gmail.com>
2021-07-23 16:32:53 +0200

    example httpget updated to perform many http requests in a loop

--- examples/httpget.c
+++ examples/httpget.c
@@ -13,31 +13,51 @@ int main(int argc, char *argv[])
 {
 	RuxcHTTPRequest v_http_request = {0};
 	RuxcHTTPResponse v_http_response = {0};
+	char *url_list[] = {
+		"http://www.kamailio.org/pub/kamailio/latest-stable-version-number",
+		"http://www.kamailio.org/pub/kamailio/latest/README",
+		"https://www.kamailio.org/pub/kamailio/latest-stable-version-number",
+		"https://www.kamailio.org/pub/kamailio/latest/README",
+		"http://www.kamailio.org/pub/kamailio/latest-stable-version-number",
+		"http://www.kamailio.org/pub/kamailio/latest/README",
+		NULL
+	};
+	int i = 0;
+	char hdrbuf[256];
 
     v_http_request.timeout = 5000;
     v_http_request.timeout_connect = 5000;
     v_http_request.timeout_read = 5000;
     v_http_request.timeout_write = 5000;
-
-	v_http_request.url = "https://www.kamailio.org/pub/kamailio/latest-stable-version-number";
-	v_http_request.url_len = strlen(v_http_request.url);
-
-	v_http_request.headers = "X-My-Key: abcdefgh\r\nX-Info: request\r\n";
-	v_http_request.headers_len = strlen(v_http_request.headers);
-
 	v_http_request.debug = 1;
+	v_http_request.reuse = 1;
 
-	ruxc_http_get(&v_http_request, &v_http_response);
+	for(i = 0; url_list[i]!=NULL; i++) {
+		v_http_request.url = url_list[i];
+		v_http_request.url_len = strlen(v_http_request.url);
 
-	if(v_http_response.retcode < 0) {
-		printf("* c:: failed to perform http get - retcode: %d\n", v_http_response.retcode);
-	} else {
-		if(v_http_response.resdata != NULL &&  v_http_response.resdata_len>0) {
-			printf("* c:: response code: %d - data len: %d - data: [%.*s]\n",
-					v_http_response.rescode, v_http_response.resdata_len,
-					v_http_response.resdata_len, v_http_response.resdata);
+		snprintf(hdrbuf, 255, "X-My-Key: KEY-%d\r\nX-Info: REQUEST-%d\r\n", i, i);
+		v_http_request.headers = hdrbuf;
+		v_http_request.headers_len = strlen(v_http_request.headers);
+
+		ruxc_http_get(&v_http_request, &v_http_response);
+
+		if(v_http_response.retcode < 0) {
+			printf("* c:: failed to perform http get [%d] - retcode: %d\n", i, v_http_response.retcode);
+		} else {
+			if(v_http_response.resdata != NULL &&  v_http_response.resdata_len>0) {
+				printf("* c:: response [%d] code: %d - data len: %d - data: [%.*s]\n", i,
+						v_http_response.rescode, v_http_response.resdata_len,
+						v_http_response.resdata_len, v_http_response.resdata);
+			}
 		}
+		ruxc_http_response_release(&v_http_response);
+
+		v_http_response.resdata = NULL;
+		v_http_response.resdata_len = 0;
+		v_http_response.retcode = 0;
+		v_http_response.rescode = 0;
 	}
-	ruxc_http_response_release(&v_http_response);
+
 	return 0;
 }
