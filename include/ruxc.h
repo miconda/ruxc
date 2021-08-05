ab5aba46df2e58fef4a35d225a33a100357ea658
Daniel-Constantin Mierla <miconda@gmail.com>
2021-08-05 09:39:56 +0200

    include/ruxc.h: C API comments

--- include/ruxc.h
+++ include/ruxc.h
@@ -3,33 +3,44 @@
 #define __LIBRUXC_H__
 
 typedef struct RuxcHTTPRequest {
-    char* url;
-    int url_len;
-    char* headers;
-    int headers_len;
-    char* data;
-    int data_len;
-    int timeout;
-    int timeout_connect;
-    int timeout_read;
-    int timeout_write;
-    int tlsmode;
-    int flags;
-    int debug;
-    int reuse;
+    char* url;           /* HTTP/S URL */
+    int url_len;         /* HTTP/S URL length */
+    char* headers;       /* Extra headers separated by \r\n */
+    int headers_len;     /* Length of extra headers */
+    char* data;          /* Data to be set as HTTP POST body */
+    int data_len;        /* Length of data */
+    int timeout;         /* Timeout in milliseconds */
+    int timeout_connect; /* Connect timeout in milliseconds */
+    int timeout_read;    /* Read timeout in milliseconds */
+    int timeout_write;   /* Write timeout in milliseconds */
+    int tlsmode;         /* TLS mode: 0 - accept all certs; 1 - accept only trusted certs */
+    int flags;           /* Internal flags - not in use yet */
+    int debug;           /* Debug mode: 1 - print debug messages to stdout */
+    int reuse;           /* Reuse connection mode: 0 - do not reuse;
+                          *   1 - single connection; 2 - connections hashmap */
 } RuxcHTTPRequest;
 
 typedef struct RuxcHTTPResponse {
-    int retcode;
-    int rescode;
-    char* resdata;
-    int resdata_len;
+    int retcode;         /* return code of processing the request */
+    int rescode;         /* HTTP response code */
+    char* resdata;       /* HTTP response data (body) */
+    int resdata_len;     /* Length of response data */
 } RuxcHTTPResponse;
 
-extern void ruxc_http_response_release(RuxcHTTPResponse *v_http_response);
+/**
+ * Perform a HTTP/S GET request
+ */
 extern int ruxc_http_get(RuxcHTTPRequest *v_http_request,
 		RuxcHTTPResponse *v_http_response);
+/**
+ * Perform a HTTP/S POST request
+ */
 extern int ruxc_http_post(RuxcHTTPRequest *v_http_request,
 		RuxcHTTPResponse *v_http_response);
+
+/**
+ * Release resources associated with a HTTP/S response
+ */
+extern void ruxc_http_response_release(RuxcHTTPResponse *v_http_response);
 
 #endif
