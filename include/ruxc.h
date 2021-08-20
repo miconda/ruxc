983d8266c1fae8acc9aad68aeb0c2a1230444bbb
Daniel-Constantin Mierla <miconda@gmail.com>
2021-08-20 11:43:42 +0200

    ruxc.h: updated comments for debug field

--- include/ruxc.h
+++ include/ruxc.h
@@ -15,7 +15,7 @@ typedef struct RuxcHTTPRequest {
     int timeout_write;   /* Write timeout in milliseconds */
     int tlsmode;         /* TLS mode: 0 - accept all certs; 1 - accept only trusted certs */
     int flags;           /* Internal flags - not in use yet */
-    int debug;           /* Debug mode: 0 - no debug; 1 - print errors; 2 - print debug */
+    int debug;           /* Debug mode: 0 - no debug; 1 - error; 2 - info; 3 - debug */
     int reuse;           /* Reuse connection mode: 0 - do not reuse;
                           *   1 - single connection; 2 - connections hashmap */
     int retry;           /* How many tries to attempt if not getting 200ok */
